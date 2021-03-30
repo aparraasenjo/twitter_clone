d346fa855bff9b5f46bbede6054a33b49e3c6974
Alberto Parra <a.parra.asenjo@gmail.com>
2021-03-30 18:50:35 -0300

    twitter DFL proyect, 2nd milestone, 4th  step corrected

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -5,8 +5,28 @@ ActiveAdmin.register User do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name, :avatar, :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
-  #
+permit_params :name, :avatar, :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :email
+    column :sign_in_count
+    column :created_at
+    column :tweet_count do |user|
+      columns user.tweets.count
+    end
+    column :followed_count do |user|
+      columns user.friends.count
+    end
+    column :likes_count do |user|
+      columns user.likes.count
+    end
+    column :retweet_count do |user|
+      columns user.tweets.pluck(:repost_id).compact!.count
+    end
+    actions
+  end
   # or
   #
   # permit_params do
