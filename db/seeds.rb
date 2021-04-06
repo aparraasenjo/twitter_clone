bc0e203b3b488e79505106dba0091a8e0bee005c
Alberto Parra <a.parra.asenjo@gmail.com>
2021-04-06 15:54:36 -0400

    last deploy to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+4.times do
+    User.create(name: Faker::Name.first_name, avatar: Faker::Avatar.image, email: Faker::Internet.email, password: "123456" )
+end
+
 200.times do
     Tweet.create(content: Faker::Lorem.sentence, user_id: [1,2,3,4].sample)
 
 end
 
-# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
